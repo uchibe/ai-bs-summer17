bd55117fdcca6fafbf14e51a3d4bfa96c6e71aee
Eiji Uchibe (uchibe-ThinkPad-X260) <uchibe@atr.jp>
2017-07-06 11:15:11 +0900

    Create a dockerfile and a start-up script.

---
+++ drun.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#docker build -t aibs/cpu:1.0 -f docker/Dockerfile .
+docker run -it --env="DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  ai/cpu:1.0 /bin/bash
+
+#docker run -it --env="DISPLAY" \
+#  --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+#  -v ${PWD}:/root/code:rw \
+#  ai/cpu:1.0 /bin/bash
+
