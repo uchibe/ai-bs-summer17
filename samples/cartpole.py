943571b6dd72cc526255eca06f3f8f8d8a3c57b8
Eiji Uchibe (uchibe-ThinkPad-X260) <uchibe@atr.jp>
2017-07-13 19:54:03 +0900

    Update the dockerfile to install OpenAI gym and some baseline RL algorithms.

---
+++ samples/cartpole.py
@@ -0,0 +1,21 @@
+import gym
+import random
+
+NUM_EPISODES = 10
+NO_OP_STEPS = 30
+
+env = gym.make('CartPole-v0')
+
+for _ in range(NUM_EPISODES):
+    terminal = False
+    observation = env.reset()
+    print(_)
+    for _ in range(random.randint(1, NO_OP_STEPS)):
+        last_observation = observation
+        observation, _, _, _ = env.step(0)
+
+    while not terminal:
+        last_observation = observation
+        action = env.action_space.sample()
+        observation, reward, terminal, _ = env.step(action)
+        env.render()
