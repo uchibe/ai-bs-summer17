ebdfee11f6c3ee74fbfa0af775a12d6859aa1bbb
Eiji Uchibe (uchibe-ThinkPad-X260) <uchibe@atr.jp>
2017-07-13 18:49:06 +0900

    Write a dockerfile to test OpenAI gym programs.

--- drun.sh
+++ drun.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-#docker build -t aibs/cpu:1.0 -f docker/Dockerfile .
-docker run -it --env="DISPLAY" \
+#docker build --force-rm=true -t aibs/cpu:1.0 -f docker/Dockerfile .
+docker run -it --init --env="DISPLAY" \
   --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  ai/cpu:1.0 /bin/bash
+  -v /etc/machine-id:/etc/machine-id:ro -v /var/run/dbus:/var/run/dbus \
+  -v ${PWD}:/root/code:rw \
+  aibs/cpu:1.0 /bin/bash
 
 #docker run -it --env="DISPLAY" \
 #  --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
