5c7506edf9593554f2abfd932f2428255409d96a
Eiji Uchibe (ncdl2) <uchibe@atr.jp>
2017-07-14 11:32:40 +0900

    Fix typo in Dockerfile and drun.sh.

--- drun.sh
+++ drun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #docker build --force-rm=true -t aibs/cpu:1.0 -f docker/Dockerfile .
-docker run -it --init --env="DISPLAY" \
+docker run -it --init --net=host --env="DISPLAY" \
   --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
   -v /etc/machine-id:/etc/machine-id:ro -v /var/run/dbus:/var/run/dbus \
-  -v ${PWD}:/root/code:rw \
+  -v ~/.Xauthority:/root/.Xauthority -v ${PWD}:/root/code:rw \
   aibs/cpu:1.0 /bin/bash
 
 #docker run -it --env="DISPLAY" \
