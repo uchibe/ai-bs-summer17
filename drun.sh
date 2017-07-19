63925cfea6d6a3dca08e7ec5d71687765ef7f493
Eiji Uchibe (uchibe-ThinkPad-X260) <uchibe@atr.jp>
2017-07-19 19:24:47 +0900

    Update the docker file for OpenAI-ROS environment.

--- drun.sh
+++ drun.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 #docker build --force-rm=true -t aibs/cpu:1.0 -f docker/Dockerfile .
-docker run -it --init --net=host --env="DISPLAY" \
-  --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  -v /etc/machine-id:/etc/machine-id:ro -v /var/run/dbus:/var/run/dbus \
-  -v ~/.Xauthority:/root/.Xauthority -v ${PWD}:/root/code:rw \
-  aibs/cpu:1.0 /bin/bash
-#docker build --force-rm=true -t aibs/ros_cpu:1.0 -f docker/rs_Dockerfile .
 #docker run -it --init --net=host --env="DISPLAY" \
 #  --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 #  -v /etc/machine-id:/etc/machine-id:ro -v /var/run/dbus:/var/run/dbus \
 #  -v ~/.Xauthority:/root/.Xauthority -v ${PWD}:/root/code:rw \
-#  aibs/ros_cpu:1.0 /bin/bash
+#  aibs/cpu:1.0 /bin/bash
+
+#docker build --force-rm=true -t aibs/ros_cpu:1.0 -f docker/rs_Dockerfile .
+docker run -it --init --net=host --env="DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v /etc/machine-id:/etc/machine-id:ro -v /var/run/dbus:/var/run/dbus \
+  -v ~/.Xauthority:/root/.Xauthority -v ${PWD}:/root/code:rw \
+  aibs/ros_cpu:1.0 /bin/bash
+
+#docker run -it --init --net=host --env="DISPLAY" \
+#  --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+#  -v /etc/machine-id:/etc/machine-id:ro -v /var/run/dbus:/var/run/dbus \
+#  -v ~/.Xauthority:/root/.Xauthority -v ${PWD}:/root/code:rw \
+#  erlerobotics/gym-gazebo:latest /bin/bash
+
 
 #nvidia-docker build --force-rm=true -t aibs/gpu:1.0 -f docker/gpu_Dockerfile .
 #nvidia-docker run -it --init --net=host --env="DISPLAY" \
---
+++ samples/test_turtlebot.py
@@ -0,0 +1,3 @@
+import gym
+env = gym.make('GazeboCircuit2TurtlebotLIDAR-v0')
+env.reset()
