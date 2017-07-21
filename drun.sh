a009a93082e14116208f40da7dc9b586dc7c533e
Eiji Uchibe (uchibe-ThinkPad-X260) <uchibe@atr.jp>
2017-07-21 20:24:05 +0900

    Fix the Docker's image tag.

--- drun.sh
+++ drun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PROJECT_NAME="ai-bs-summer17"
+
 #docker build --force-rm=true -t uchibe/cpu:1.0 -f docker/Dockerfile .
 #docker run -it --init --net=host --env="DISPLAY" \
 #  --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
@@ -7,12 +9,21 @@
 #  -v ~/.Xauthority:/root/.Xauthority -v ${PWD}:/root/ai-bs-summer17:rw \
 #  uchibe/cpu:1.0 /bin/bash
 
-#docker build --force-rm=true -t uchibe/aibs_ros_cpu:1.0 -f docker/rs_Dockerfile .
+#docker build --force-rm=true -t uchibe/aibs_ros_cpu:1.0 -f docker/Dockerfile .
+#       --user=$(id -u):$(id -g) \
+#       --workdir="/home/$USER" \
+#      --volume="/etc/group:/etc/group:ro" \
+#       --volume="/etc/passwd:/etc/passwd:ro" \
+#       --volume="/etc/shadow:/etc/shadow:ro" \
+#       --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
 docker run -it --init --net=host --env="DISPLAY" \
-  --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  -v /etc/machine-id:/etc/machine-id:ro -v /var/run/dbus:/var/run/dbus \
-  -v ~/.Xauthority:/root/.Xauthority -v ${PWD}:/root/ai-bs-summer17:rw \
-  uchibe/aibs_ros_cpu:1.0 /bin/bash
+       --env="QT_X11_NO_MITSHM=1" \
+       --volume="${PWD}:/home/$USER/$PROJECT_NAME" \
+       --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+       --volume="/etc/machine-id:/etc/machine-id:ro" \
+       --volume="/var/run/dbus:/var/run/dbus" \
+       --volume="/home/$USER/.Xauthority:/home/$USER/.Xauthority" \
+       uchibe/aibs_ros_cpu:1.0 /bin/bash
 
 #docker run -it --init --net=host --env="DISPLAY" \
 #  --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
